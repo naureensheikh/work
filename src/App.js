720f30746c80627225a57fb72cd35f9f1061825e
Naureen Sheikh <naureensheikh@Naureens-MacBook-Pro.local>
2023-11-05 20:12:09 +0400

    test

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
 					{/* <AppHeader /> */}
 					<Suspense fallback={""}>
 						<Routes>
-							<Route path="/" element={<Home />} />
+							<Route path="https://naureensheikh.github.io/work" element={<Home />} />
 							<Route path="projects" element={<Projects />} />
 							<Route
 								path="projects/single-project"
