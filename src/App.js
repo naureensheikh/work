32ad4382177bf292005472864747a6b40b4a1ba3
Naureen Sheikh <naureensheikh@Naureens-MacBook-Pro.local>
2023-11-05 19:17:45 +0400

    github pages setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+import './css/App.css';
 import { AnimatePresence } from 'framer-motion';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import AppFooter from './components/shared/AppFooter';
-import AppHeader from './components/shared/AppHeader';
-import './css/App.css';
+import Home from './pages/Home';
 import UseScrollToTop from './hooks/useScrollToTop';
 
 const About = lazy(() => import('./pages/AboutMe'));
 const Contact = lazy(() => import('./pages/Contact.jsx'));
-const Home = lazy(() => import('./pages/Home'));
+// const Home = lazy(() => import('./pages/Home'));
 const Projects = lazy(() => import('./pages/Projects'));
 const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
 
